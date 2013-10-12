37d1ed8efb6d5b7d6c16116addc90ebc0c140a28
gracikjw <JamesGracik@gmail.com>
2013-10-12 10:52:23 -0400

    New Shells
    I added a shells controller and view

---
+++ Sand/Sand/Controllers/shells.vb
@@ -0,0 +1,13 @@
+﻿Namespace Sand
+    Public Class shells
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /shells
+
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace
