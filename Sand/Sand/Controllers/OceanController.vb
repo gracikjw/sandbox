dff37c7e0b531d0a1072f62dba05f9294535c006
ragsdalewn <ragsdalewn@goldmail.etsu.edu>
2013-10-13 14:40:00 -0400

    Added an Ocean

---
+++ Sand/Sand/Controllers/OceanController.vb
@@ -0,0 +1,13 @@
+﻿Namespace Sand
+    Public Class OceanController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /Ocean
+
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace
