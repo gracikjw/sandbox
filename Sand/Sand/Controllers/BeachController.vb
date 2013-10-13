ef62d0ce1704858e8a62da37574174c7d3a6eeac
ragsdalewn <ragsdalewn@goldmail.etsu.edu>
2013-10-13 14:45:57 -0400

    New Beach

---
+++ Sand/Sand/Controllers/BeachController.vb
@@ -0,0 +1,13 @@
+﻿Namespace Sand
+    Public Class BeachController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /Beach
+
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace
